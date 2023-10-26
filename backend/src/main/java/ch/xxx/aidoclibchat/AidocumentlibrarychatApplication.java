ffeae94d6a1c7d511dae07628fcb82ffadd10bff
Angular2guy <angular2guy@gmail.com>
2023-10-26 18:00:30 +0200

    feat: first step

---
+++ backend/src/main/java/ch/xxx/aidoclibchat/AidocumentlibrarychatApplication.java
@@ -0,0 +1,13 @@
+package ch.xxx.aidoclibchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidocumentlibrarychatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AidocumentlibrarychatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ch/xxx/aidocumentlibrarychat/AidocumentlibrarychatApplicationTests.java
@@ -0,0 +1,12 @@
+package ch.xxx.aidocumentlibrarychat;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AidocumentlibrarychatApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
