55a436ed7a7beef409b96eda5102e0317a6817e6
Angular2guy <angular2guy@gmail.com>
2023-11-01 20:35:46 +0100

    feat: add material

--- frontend/src/angular/src/app/app.config.ts
+++ frontend/src/angular/src/app/app.config.ts
@@ -14,7 +14,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideNoopAnimations()]
 };
