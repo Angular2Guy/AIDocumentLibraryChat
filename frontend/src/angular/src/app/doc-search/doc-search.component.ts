526313d29bf15c10dce0a228b57b032200de5040
Angular2guy <angular2guy@gmail.com>
2023-11-11 10:07:46 +0100

    feat: doc search

--- frontend/src/angular/src/app/doc-search/doc-search.component.ts
+++ frontend/src/angular/src/app/doc-search/doc-search.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+/**
+ *    Copyright 2023 Sven Loesekann
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+import { Component, DestroyRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-doc-search',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,MatToolbarModule,MatButtonModule,MatTableModule],
   templateUrl: './doc-search.component.html',
   styleUrls: ['./doc-search.component.scss']
 })
-export class DocSearchComponent {
-
+export class DocSearchComponent {        
+    constructor(private destroyRef: DestroyRef, private router: Router) { }
+    
+	protected showList(): void {
+		this.router.navigate(['/doclist']);
+	}
+	
+	protected logout(): void {
+		console.log('logout');
+	}
 }
