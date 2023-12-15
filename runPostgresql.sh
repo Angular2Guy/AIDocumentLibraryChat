5ae864d2455c5c4501d09b1fef9637b98312051f
Angular2guy <angular2guy@gmail.com>
2023-12-15 08:27:14 +0100

    feat: ollama

--- runPostgresql.sh
+++ runPostgresql.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 docker pull ankane/pgvector:latest
 docker run --name aidoclibchat-postgres -e POSTGRES_PASSWORD=sven1 -e POSTGRES_USER=sven1 -e POSTGRES_DB=aidoclibchat -p 5432:5432 -d ankane/pgvector
+docker run --name aidoclibchat-postgres-ollama -e POSTGRES_PASSWORD=sven1 -e POSTGRES_USER=sven1 -e POSTGRES_DB=aidoclibchat -p 5432:5432 -d ankane/pgvector
 # docker start aidoclibchat-postgres
 # docker stop aidoclibchat-postgres
+# docker start aidoclibchat-postgres-ollama
+# docker stop aidoclibchat-postgres-ollama
