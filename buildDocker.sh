5cda6dbece28fc94e8efa971c33579993c257611
Angular2guy <angular2guy@gmail.com>
2024-01-06 09:10:26 +0100

    feat: add docker image

--- buildDocker.sh
+++ buildDocker.sh
@@ -2,5 +2,5 @@
 #./gradlew clean build -PwithAngular=true -PuseChromium=true -PuseOllama=true
 #./gradlew clean build -PwithAngular=true -PuseChromium=true
 ./gradlew clean build -PwithAngular=true
-docker build -t angular2guy/aidocumentlibrarychat:latest --build-arg APP_FILE=build/libs/angularAndSpringWithMaps.jar --no-cache .
+docker build -t angular2guy/aidocumentlibrarychat:latest --build-arg JAR_FILE=aidocumentlibrarychat.jar --no-cache .
 docker run -p 8080:8080 --memory="384m" --network="host" angular2guy/aidocumentlibrarychat:latest
