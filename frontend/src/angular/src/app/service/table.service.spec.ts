4a341f49accfd58ecad5025e4038e9e267393545
Angular2guy <angular2guy@gmail.com>
2024-02-11 11:51:45 +0100

    fix: build

--- frontend/src/angular/src/app/service/table.service.spec.ts
+++ frontend/src/angular/src/app/service/table.service.spec.ts
@@ -13,12 +13,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TableService } from './table.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('TableService', () => {
   let service: TableService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({providers: [HttpClient, HttpHandler]});
     service = TestBed.inject(TableService);
   });
 
