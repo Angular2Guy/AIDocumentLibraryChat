6419ceae3d7324434dde9c93d3b8873ecdce9f4f
Angular2guy <angular2guy@gmail.com>
2024-02-11 10:34:23 +0100

    feat: table search next step

--- frontend/src/angular/src/app/table-search/table-search.component.ts
+++ frontend/src/angular/src/app/table-search/table-search.component.ts
@@ -10,17 +10,45 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  */
-import { Component } from '@angular/core';
+import { Component, DestroyRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableService } from '../service/table.service';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormControl, FormsModule,ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'; 
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-table-search',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,MatToolbarModule,MatButtonModule,MatTableModule,MatInputModule,
+  	MatFormFieldModule,FormsModule,ReactiveFormsModule,MatProgressSpinnerModule],
   templateUrl: './table-search.component.html',
   styleUrl: './table-search.component.scss'
 })
 export class TableSearchComponent {
-	constructor(private tableService: TableService) { }
+	protected searchValueControl = new FormControl('', [Validators.required, Validators.minLength(3)]);
+	protected searching = false;
+	protected requestFailed = false;
+	protected msWorking = 0;
+	private repeatSub: Subscription | null = null;
+	
+	constructor(private destroyRef: DestroyRef, private router: Router, private tableService: TableService) { }
+	
+	protected search(): void {
+		console.log('search');
+	}
+	
+	protected showList(): void {
+		this.router.navigate(['/doclist']);
+	}
+	
+	protected logout(): void {
+		console.log('logout');
+	}
 }
