9d2e750c1632d098b8637b8de5e7a1c7845291cc
Angular2guy <angular2guy@gmail.com>
2024-12-19 17:30:13 +0100

    feat: use better embedding model

--- runPostgresql.sh
+++ runPostgresql.sh
@@ -2,7 +2,10 @@
 docker pull ankane/pgvector:latest
 docker run --name aidoclibchat-postgres -e POSTGRES_PASSWORD=sven1 -e POSTGRES_USER=sven1 -e POSTGRES_DB=aidoclibchat -p 5432:5432 -d ankane/pgvector
 docker run --name aidoclibchat-postgres-ollama -e POSTGRES_PASSWORD=sven1 -e POSTGRES_USER=sven1 -e POSTGRES_DB=aidoclibchat -p 5432:5432 -d ankane/pgvector
+docker run --name aidoclibchat-postgres-ollama2 -e POSTGRES_PASSWORD=sven1 -e POSTGRES_USER=sven1 -e POSTGRES_DB=aidoclibchat -p 5432:5432 -d ankane/pgvector
 # docker start aidoclibchat-postgres
 # docker stop aidoclibchat-postgres
 # docker start aidoclibchat-postgres-ollama
 # docker stop aidoclibchat-postgres-ollama
+# docker start aidoclibchat-postgres-ollama2
+# docker stop aidoclibchat-postgres-ollama2
